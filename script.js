1f0713f75d9699d4a6ca80c2114714157117ea60
naina0112 <sharmanaina0237@gmail.com>
2025-03-18 22:15:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,77 @@
-let timerDisplay = document.querySelector('.timerDisplay');
-let stopBtn = document.getElementById('stopBtn');
-let startBtn = document.getElementById('startBtn');
-let resetBtn = document.getElementById('resetBtn');
-
-let msec = 00;
-let secs = 00;
+let seconds = 00;
+let tens = 00;
 let mins = 00;
+let getSeconds = document.querySelector('.seconds');
+let getTens = document.querySelector('.tens');
+let getMins = document.querySelector('.mins');
+let btnStart = document.querySelector('.btn-start');
+let btnStop = document.querySelector('.btn-stop');
+let btnReset = document.querySelector('.btn-reset');
+let btnLapping = document.querySelector('.btn-lapping');
+let interval;
 
-let timerId = null;
-
-startBtn.addEventListener('click', function(){
-    if(timerId !== null){
-        clearInterval(timerId);
-    }
-    timerId = setInterval(startTimer, 10);
-});
-
-stopBtn.addEventListener('click', function(){
-    clearInterval(timerId);
-});
-
-resetBtn.addEventListener('click', function(){
-    clearInterval(timerId);
-    timerDisplay.innerHTML = `00 : 00 : 00`;
-    msec = secs = mins = 00;
-});
+btnStart.addEventListener('click', () => {
+    clearInterval(interval);
+    inverval = setInterval(startTimer, 10);
+})
+btnStop.addEventListener('click', () => {
+    clearInterval(inverval);
+})
+btnReset.addEventListener('click', () => {
+    clearInterval(inverval);
+    tens = '00';
+    seconds = '00';
+    mins = '00';
+    getSeconds.innerHTML = seconds;
+    getTens.innerHTML = tens;
+    getMins.innerHTML = mins;
+})
 
 function startTimer(){
-    msec++;
-    if(msec == 100){
-        msec = 0;
-        secs++;
-        if(secs == 60){
-            secs = 0;
-mins++;
-        }
+    tens++;
+    if(tens <= 9){
+        getTens.innerHTML = '0' + tens;
     }
-
-    let msecString = msec < 10 ? `0${msec}` : msec;
-    let secsString = secs < 10 ? `0${secs}` : secs;
-    let minsString = mins < 10 ? `0${mins}` : mins;
-    
-
-    timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
-
+    if(tens > 9){
+        getTens.innerHTML = tens;
+    }
+    if(tens > 99){
+        seconds++;
+        getSeconds.innerHTML = '0' + seconds;
+        tens = 0;
+        getTens.innerHTML = '0' + 0;
+    }
+    if(seconds > 9){
+        getSeconds.innerHTML = seconds;
+    }
+    if(seconds > 59){
+        mins++;
+        getMins.innerHTML = '0' + mins;
+        seconds = 0;
+        getSeconds.innerHTML = '0' + 0;
+    }
+    if(mins > 9){
+        getSeconds.innerHTML = mins;
+    }
 }
+
+function recordLap() {
+    if (running) {
+        lapCount++;
+        const lapTime = formatTime(difference);
+        const lapItem = document.createElement("li");
+        lapItem.textContent = `Lap ${lapCount}: ${lapTime}`;
+        lapList.appendChild(lapItem);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
